b40e44148a1853b432b1124259b260267cd211cd
izenat <ize.nat07@gmail.com>
2025-09-04 16:26:06 -0300

    n8n - Remove clientes da lista CONSULTORES_CLIENTES

--- formulario_relatorio_mensal.py
+++ formulario_relatorio_mensal.py
@@ -79,8 +79,7 @@ CONSULTORES_CLIENTES = {
             "OPT.DOC. Gestão de Consultórios"
         ],
         "Nath Toledo": [
-            "Grupo RedeSul","Ugor LTDA","Bela"
-        ],
+            "Grupo RedeSul","Ugor LTDA",],
         "William Alves da Silva": [
             "Doutor 7",
             "TOKLAR",
