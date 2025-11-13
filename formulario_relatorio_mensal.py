9ee75cbfae6bfd8c940f929627cc9faabdb7e75b
izenat <ize.nat07@gmail.com>
2025-11-13 09:09:25 -0300

    n8n - Remove clientes da lista CONSULTORES_CLIENTES

--- formulario_relatorio_mensal.py
+++ formulario_relatorio_mensal.py
@@ -325,7 +325,7 @@ CONSULTORES_CLIENTES = {
         "Rayane Caroline Cândida de Amorim Oliveira": [
             "AR Advocacia Empresarial",
             "EG Transportes e Logísticas",
-            "MACARRONADA ITALIANA",
+
             "Peterson & Escobar ADV",
             "Projector",
 
